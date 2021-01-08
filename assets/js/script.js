de73a769c260cc9366e325e3c88f6cd5bbc0082e
Israel Molestina <molestinaisrael@gmail.com>
2021-01-08 12:39:48 -0500

    made arrays for each user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// Arrays for each option user can choos for thier password
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "{", "}", "|", "[", "]", ";", "<", ">", "?", "/"];
+/*
 var userLength = prompt("Please choose desired length of your password (must be between 8 and 128)");
 
 if (userLength < 8 || userLength > 128) {
@@ -17,6 +22,7 @@ if (userLength > 8 && userLength < 128) {
 if (userInputUpper !== true && userInputLower !== true && userInputNum !== true && userInputSpec !== true) {
   alert("You must select at least one character type")
 }
+*/
 /*
 // variables for possible pasword characthers
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
