7a592d59b542d64c304f9f040f99bc5638dcba87
Israel Molestina <molestinaisrael@gmail.com>
2021-01-08 18:59:40 -0500

    put function within else if statemnts but its still not working :(

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,59 +2,74 @@
 var generateBtn = document.querySelector("#generate");
 
 // Arrays for each option user can choos for thier password
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-var numeric = ["01234567890123456789"];
-var specialChar = ["~!@#$%^&*()_+-={}|[];<>?/"];
-var userInput = [""];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "{", "}", "|", "[", "]", ";", "<", ">", "?", "/"];
+var userInput = [];
 var userPassword = "";
 
 function generatePassword() {
 
   // Asks what length user wants the passwords and adds to userInput
   var userLength = prompt("Please choose desired length of your password (must be between 8 and 128)");
-
+  var userLePar = parseInt(userLength);
   // Validates user response is less than more than 8 and less than 128
-  while (userLength < 8 || userLength > 128) {
+  if (userLength < 8 || userLength > 128) {
     var userLength = prompt("Please choose a number between 8 and 128");
   }
 
-  var userLePar = parseInt(userLength);
+  else if  (Number.isNaN(userLePar)) {
+    var userLength = prompt("Please choose a number between 8 and 128");
+  }
+  else if (userLength >= 8 && userLength <= 128) {
+    var userInputUpper = confirm("Would you like your password to contain upper case letter?");
+    if (userInputUpper) {
+      userInput = userInput.concat(upperCase);
+    }
+  
+    var userInputLower = confirm("Would you like your password to contain lower case letters?");
+    if (userInputLower) {
+      userInput = userInput.concat(lowerCase);
+    }
+  
+    var userInputNum = confirm("Would you like your password to contain numbers?");
+    if (userInputNum) {
+      userInput = userInput.concat(numeric);
+    }
+  
+    var userInputSpec = confirm("Would you like your password to contain special characters?");
+    if (userInputSpec) {
+      userInput = userInput.concat(specialChar);
+    }
+  
+    // Validates that user chose at least one character
+    if (userInputUpper === false && userInputLower === false && userInputNum === false && userInputSpec === false) {
+      alert("must choose at least one character type")
+      return userPassword = ""
+    }
+  }
 
+  else {
+    return userPassword = ""
+  }
+
+  
+  /*
   // Validates user response is a number
   while (Number.isNaN(userLePar)) {
     var userLength = prompt("Please choose a number between 8 and 128");
-  }
+  }*/
 
   // Following block of codes confirms what characters user wants to include in password
-  var userInputUpper = confirm("Would you like your password to contain upper case letter?");
-  if (userInputUpper) {
-    userInput += upperCase;
-  }
-
-  var userInputLower = confirm("Would you like your password to contain lower case letters?");
-  if (userInputLower) {
-    userInput += lowerCase;
-  }
-
-  var userInputNum = confirm("Would you like your password to contain numbers?");
-  if (userInputNum) {
-    userInput += numeric;
-  }
-
-  var userInputSpec = confirm("Would you like your password to contain special characters?");
-  if (userInputSpec) {
-    userInput += specialChar;
-  }
-
-  // Validates that user chose at least one character
-  if (userInputUpper === false && userInputLower === false && userInputNum === false && userInputSpec === false) {
-    alert("must choose at least one character type")
-  }
+ 
   
   // for loop that chooses random characters in the arrays user chose
   for (var i =0; i < userLength; ++i) {
-    userPassword += userInput.charAt(Math.floor(Math.random() * userInput.length));
+    var index = Math.floor(Math.random() * userInput.length);
+    userPassword += userInput[index] + "";
+
+    console.log(userPassword)
   }
 
   return userPassword
