1c982ea7b132652ba3b2cc0a543a0494025ee284
Israel Molestina <molestinaisrael@gmail.com>
2021-01-08 14:43:45 -0500

    made confirms for user input of what characters they would like and if statments to add the input to an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 // Arrays for each option user can choos for thier password
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "{", "}", "|", "[", "]", ";", "<", ">", "?", "/"];
+var userInput = "";
+
+function generatePassword() {
+
+  var userLength = prompt("Please choose desired length of your password (must be between 8 and 128)");
+  if (userLength < 8 || userLength > 128) {
+    var userLength = prompt("Please choose a number between 8 and 128");
+  }
+  else (userLength > 8 || userLength < 128) {
+    userLength.concat(userInput);
+  }
+
+  var userInputUpper = confirm("Would you like your password to contain upper case letter?");
+  if (userInputUpper === true) {
+    userInputUpper.concat(userInput);
+  }
+
+  var userInputLower = confirm("Would you like your password to contain lower case letters?");
+  if (userInputLower === true) {
+    userInputLower.concat(userInput);
+  }
+
+  var userInputNum = confirm("Would you like your password to contain numbers?");
+  if (userInputNum === true) {
+    userInputNum.concat(userInput);
+  }
+
+  var userInputSpec = confirm("Would you like your password to contain special characters?");
+  if (userInputSpec === true) {
+    userInputSpec.concat(userInput);
+  }
+
+
+}
+
+
 /*
 var userLength = prompt("Please choose desired length of your password (must be between 8 and 128)");
 
@@ -76,6 +113,7 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
